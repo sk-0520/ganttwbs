07e5ecd61ff745c8a44b20f2a0863ec0b66c2d89
sk <content.type.text.net@gmail.com>
2023-02-18 20:08:32 +0900

    初期構築

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,13 +1,15 @@
 import { Html, Head, Main, NextScript } from 'next/document'
 
 export default function Document() {
-  return (
-    <Html lang="en">
-      <Head />
-      <body>
-        <Main />
-        <NextScript />
-      </body>
-    </Html>
-  )
+	const language = process.env.NEXT_PUBLIC_APP_LANGUAGE;
+
+	return (
+		<Html lang={language}>
+			<Head />
+			<body>
+				<Main />
+				<NextScript />
+			</body>
+		</Html>
+	)
 }
