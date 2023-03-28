02427e53f0f12f1964ce27fda01c930fc7f475f8
sk <content.type.text.net@gmail.com>
2023-03-28 21:51:08 +0900

    ValueUnit

--- models/data/Design.ts
+++ models/data/Design.ts
@@ -1,9 +1,19 @@
 
+/**
+ * 単位付きの値。
+ */
+export interface ValueUnit {
+	value: number;
+	unit: 'px' | string;
+}
+
 export interface Design {
 	cell: {
 		maxWidth: string;
 		minWidth: string;
 		maxHeight: string;
 		minHeight: string;
+		width: ValueUnit;
+		height: ValueUnit;
 	}
 }
