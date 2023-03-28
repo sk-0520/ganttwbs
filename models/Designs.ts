41db7b60ae39db521294f772fdda38200efed600
sk <content.type.text.net@gmail.com>
2023-03-29 00:15:34 +0900

    名前が悪いわ

--- models/Designs.ts
+++ models/Designs.ts
@@ -2,7 +2,6 @@ import { isValueUnit, ValueUnit } from "./data/Design";
 
 // セレクタとか難しい概念は知らん
 
-type ClassName = string;
 type Property = string;
 type Value = string;
 
@@ -16,33 +15,36 @@ export abstract class Designs {
 		return `${valueUnit.value}${valueUnit.unit}`;
 	}
 
-	public static convertStyleClasses(obj: object, parents: ReadonlyArray<string>): Map<ClassName, Map<Property, Value>> {
-		// const result = new Map<ClassName, Map<Property, Value>>();
+	public static convertStyleClasses(obj: object, parents: ReadonlyArray<string>): Map<string, Map<Property, Value>> {
+		const result = new Map<string, Map<Property, Value>>();
 
-		// for (const [className, propertiesOrNestedClass] of Object.entries(obj)) {
-		// 	const properties = new Map<Property, Value>();
+		const properties = new Map<Property, Value>();
 
-		// 	if (typeof (propertiesOrNestedClass) === 'object') {
-		// 		if (isValueUnit(propertiesOrNestedClass)) {
-		// 			// result.push({
-		// 			// 	className: key,
-		// 			// 	properties: {
-		// 			// 		[key]: Designs.toProperty(vu)
-		// 			// 	}
-		// 			// });
-		// 		} else {
-		// 			const map = this.convertStyleClasses(propertiesOrNestedClass, [...parents, className]);
-		// 			for (const [key, value] of map) {
-		// 				result.set(key, value);
-		// 			}
-		// 			continue;
-		// 		}
-		// 	} else {
-		// 		properties.set(className, propertiesOrNestedClass);
-		// 	}
-		// }
+		for (const [className, valuesOrNestedBlock] of Object.entries(obj)) {
+			if (typeof (valuesOrNestedBlock) === 'object') {
+				if (isValueUnit(valuesOrNestedBlock)) {
+					// result.push({
+					// 	className: key,
+					// 	properties: {
+					// 		[key]: Designs.toProperty(vu)
+					// 	}
+					// });
+				} else {
+					const map = this.convertStyleClasses(valuesOrNestedBlock, [...parents, className]);
+					for (const [key, value] of map) {
+						result.set(key, value);
+					}
+				}
+			} else {
+				properties.set(className, valuesOrNestedBlock);
+			}
+		}
 
-		// return result;
+		if (properties.size) {
+			result.set(parents.join("_"), properties);
+		}
+
+		return result;
 	}
 
 
