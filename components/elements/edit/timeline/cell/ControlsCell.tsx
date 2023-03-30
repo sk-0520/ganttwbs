c86d02d0afff4447ec451427736d8b1ca9841557
sk <content.type.text.net@gmail.com>
2023-03-30 15:37:33 +0900

    コンポーネント作るかぁ

--- components/elements/edit/timeline/cell/ControlsCell.tsx
+++ components/elements/edit/timeline/cell/ControlsCell.tsx
@@ -24,65 +24,62 @@ const Component: NextPage<Props> = (props: Props) => {
 			"timeline-controls"
 			+ " " + (visibleControls ? " prompt" : "")
 		}>
-			{visibleControls ? (
+			<button
+				onClick={handleStartControls}
+			>
+				CTRL
+			</button>
+			{visibleControls && (
 				<>
-					<div className="tools before">
-						<div className="overlay">
-							<ul>
-								<li>
-									<button
-										className="simple"
-										disabled={props.disabled}
-										onClick={_ => props.moveItem("up")}>
-										⬆️上へ移動
-									</button >
-								</li >
-								<li>
-									<button
-										className="simple"
-										disabled={props.disabled}
-										onClick={_ => props.moveItem("down")}>
-										⬇️下へ移動
-									</button>
-								</li>
-								<li>
-									<button
-										className="simple"
-										disabled={props.disabled}
-										onClick={_ => props.addItem("group")}
-									>
-										{props.currentTimelineKind === "group" ? "📂" : "📁"}
-										直下にグループ追加
-									</button>
-								</li>
-								<li>
-									<button
-										className="simple"
-										disabled={props.disabled}
-										onClick={_ => props.addItem("task")}
-									>
-										{props.currentTimelineKind === "group" ? "🐜" : "🐞"}
-										直下にタスク追加
-									</button>
-								</li>
-								<li>
-									<button
-										className="simple"
-										disabled={props.disabled}
-										onClick={_ => props.deleteItem()}>
-										🗑️削除
-									</button>
-								</li>
-							</ul >
-						</div>
+					<div className="tools after">
+						<ul>
+							<li>
+								<button
+									className="simple"
+									disabled={props.disabled}
+									onClick={_ => props.moveItem("up")}>
+									⬆️上へ移動
+								</button >
+							</li >
+							<li>
+								<button
+									className="simple"
+									disabled={props.disabled}
+									onClick={_ => props.moveItem("down")}>
+									⬇️下へ移動
+								</button>
+							</li>
+							<li>
+								<button
+									className="simple"
+									disabled={props.disabled}
+									onClick={_ => props.addItem("group")}
+								>
+									{props.currentTimelineKind === "group" ? "📂" : "📁"}
+									直下にグループ追加
+								</button>
+							</li>
+							<li>
+								<button
+									className="simple"
+									disabled={props.disabled}
+									onClick={_ => props.addItem("task")}
+								>
+									{props.currentTimelineKind === "group" ? "🐜" : "🐞"}
+									直下にタスク追加
+								</button>
+							</li>
+							<li>
+								<button
+									className="simple"
+									disabled={props.disabled}
+									onClick={_ => props.deleteItem()}>
+									🗑️削除
+								</button>
+							</li>
+						</ul >
 					</div>
 				</>
-			) : (
-				<button
-					onClick={handleStartControls}
-				>
-					CTRL
-				</button>
 			)}
 		</div>
 	)
