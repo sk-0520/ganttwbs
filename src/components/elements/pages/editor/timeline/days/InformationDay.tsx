28de8e731be3c29a5ee29be362d8b1a31732c83a
sk <content.type.text.net@gmail.com>
2023-05-07 13:32:59 +0900

    メンバー・タイムライン情報をソート

--- src/components/elements/pages/editor/timeline/days/InformationDay.tsx
+++ src/components/elements/pages/editor/timeline/days/InformationDay.tsx
@@ -3,10 +3,12 @@ import { FC } from "react";
 import { useLocale } from "@/locales/locale";
 import { Calendars } from "@/models/Calendars";
 import { DayInfo } from "@/models/data/DayInfo";
+import { MemberGroupPair } from "@/models/data/MemberGroupPair";
 import { CalendarInfoProps } from "@/models/data/props/CalendarInfoProps";
 import { ResourceInfoProps } from "@/models/data/props/ResourceInfoProps";
 import { SettingProps } from "@/models/data/props/SettingProps";
 import { TimelineStoreProps } from "@/models/data/props/TimelineStoreProps";
+import { AnyTimeline } from "@/models/data/Setting";
 import { DateTime } from "@/models/DateTime";
 import { Days } from "@/models/Days";
 import { Strings } from "@/models/Strings";
@@ -27,6 +29,7 @@ const InformationDay: FC<Props> = (props: Props) => {
 		duplicateMembers: new Set(),
 		targetTimelines: new Set(),
 	};
+
 	const nextDay = props.date.add(1, "day");
 	for (const [ticks, info] of props.timelineStore.dayInfos) {
 		if (props.date.ticks <= ticks && ticks < nextDay.ticks) {
@@ -38,6 +41,34 @@ const InformationDay: FC<Props> = (props: Props) => {
 			}
 		}
 	}
+
+	// ソート済み重複メンバー取得
+	const sortedMembers = [...mergedDayInfo.duplicateMembers]
+		.map(a => props.resourceInfo.memberMap.get(a) as MemberGroupPair)
+		.sort((a, b) => {
+			const groupIndex = props.resourceInfo.groupItems.indexOf(a.group);
+			const groupCompare = groupIndex - props.resourceInfo.groupItems.indexOf(b.group);
+			if (!groupCompare) {
+				return groupCompare;
+			}
+			const group = props.resourceInfo.groupItems[groupIndex];
+			const members = props.resourceInfo.memberItems.get(group);
+			if (!members) {
+				throw new Error();
+			}
+			return members.indexOf(a.member) - members.indexOf(b.member);
+		})
+		;
+
+	// ソート済みタイムライン取得
+	const sortedTimelines = [...mergedDayInfo.targetTimelines]
+		.map(a => props.timelineStore.totalItemMap.get(a) as AnyTimeline)
+		.sort((a, b) => {
+			const aIndex = props.timelineStore.indexItemMap.get(a.id) ?? -1; // TODO: 落とした方がいいかも
+			const bIndex = props.timelineStore.indexItemMap.get(b.id) ?? -1;
+			return aIndex - bIndex;
+		});
+
 
 	return (
 		<td
@@ -59,21 +90,17 @@ const InformationDay: FC<Props> = (props: Props) => {
 							</dt>
 							<dd>
 								<ul>
-									{[...mergedDayInfo.duplicateMembers].map(b => {
-										const member = props.resourceInfo.memberMap.get(b);
-										if (!member) {
-											throw new Error();
-										}
+									{sortedMembers.map(a => {
 										return (
 											<li
-												key={member.member.id}
-												title={member.member.id}
+												key={a.member.id}
+												title={a.member.id}
 											>
 												{Strings.replaceMap(
 													locale.pages.editor.timeline.information.memberFormat,
 													{
-														"MEMBER": member.member.name,
-														"GROUP": member.group.name,
+														"MEMBER": a.member.name,
+														"GROUP": a.group.name,
 													}
 												)}
 											</li>
@@ -87,20 +114,15 @@ const InformationDay: FC<Props> = (props: Props) => {
 							</dt>
 							<dd>
 								<ul>
-									{[...mergedDayInfo.targetTimelines].map(b => {
-										const timeline = props.timelineStore.totalItemMap.get(b);
-										if (!timeline) {
-											throw new Error();
-										}
-
+									{sortedTimelines.map(i => {
 										return (
 											<li
-												key={timeline.id}
-												title={timeline.id}
+												key={i.id}
+												title={i.id}
 											>
-												{Timelines.toIndexNumber(props.timelineStore.calcDisplayId(timeline))}
+												{Timelines.toIndexNumber(props.timelineStore.calcDisplayId(i))}
 												:
-												{timeline.subject}
+												{i.subject}
 											</li>
 										);
 									})}
