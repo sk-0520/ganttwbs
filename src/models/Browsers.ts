c26e976a16db3e3aebc48055a2588b71478bf61c
sk <content.type.text.net@gmail.com>
2023-05-08 18:14:04 +0900

    タイムゾーン取得はブラウザ限定

--- src/models/Browsers.ts
+++ src/models/Browsers.ts
@@ -1,4 +1,22 @@
+import { Types } from "@/models/Types";
+
 export abstract class Browsers {
+
+	/**
+	 * ブラウザで実行中か。
+	 */
+	public static get running(): boolean {
+		return !Types.isUndefined(global.window);
+	}
+
+	/**
+	 * ブラウザで実行されている子を強制。
+	 */
+	public static enforceRunning(): void {
+		if(!this.running) {
+			throw new Error("not running in the browser");
+		}
+	}
 
 	public static download(fileName: string, blob: Blob): void {
 		const anchorElement = document.createElement("a");
