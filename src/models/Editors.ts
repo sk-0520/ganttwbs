276340b2759674f32ebd91108eb5a3ee3198bb1f
sk <content.type.text.net@gmail.com>
2023-05-15 19:36:34 +0900

    タイムラインの表示移動処理は中央をベースにした

--- src/models/Editors.ts
+++ src/models/Editors.ts
@@ -5,17 +5,38 @@ import { TimelineIdOrObject, Timelines } from "@/models/Timelines";
 export abstract class Editors {
 
 	public static scrollView(timeline: TimelineIdOrObject | undefined, date: DateTime | undefined): void {
-		const mainContentElement = document.querySelector(".tab-timeline");
-		if (!mainContentElement) {
-			return;
+		if (!timeline && !date) {
+			throw new Error();
 		}
+
+		const mainContentElement = Dom.querySelector(".tab-timeline");
+		const crossHeaderElement = Dom.getElementById("cross-header");
 
 		const columnElement = date ? Dom.getElementById(Timelines.toDaysId(date)) : undefined;
 		const rowElement = timeline ? Dom.getElementById(Timelines.toRowId(timeline)) : undefined;
 
+		const timelineSize = {
+			width: mainContentElement.clientWidth - crossHeaderElement.clientWidth,
+			height: mainContentElement.clientHeight - crossHeaderElement.clientHeight,
+		};
+
 		mainContentElement.scrollTo({
-			left: columnElement?.offsetLeft,
-			top: rowElement?.offsetTop,
+			left: columnElement
+				? (
+					0 < timelineSize.width
+						? columnElement.offsetLeft - (timelineSize.width / 2)
+						: columnElement.offsetLeft
+				)
+				: undefined
+			,
+			top: rowElement
+				? (
+					0 < timelineSize.height
+						? rowElement.offsetTop - (timelineSize.height / 2)
+						: rowElement.offsetTop
+				)
+				: undefined
+			,
 		});
 	}
 
