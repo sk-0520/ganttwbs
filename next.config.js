b0453ee004ef5a3681636c868d9a38677151823e
sk <content.type.text.net@gmail.com>
2024-07-11 21:42:29 +0900

    compiler.removeConsole

--- next.config.js
+++ next.config.js
@@ -18,6 +18,12 @@ const nextConfig = {
 	eslint: {
 		ignoreDuringBuilds: true,
 	},
+	compiler: {
+		removeConsole:
+			process.env.NODE_ENV === "production"
+				? { exclude: ["info", "warn", "error"] }
+				: false,
+	},
 };
 
 module.exports = withBundleAnalyzer(nextConfig);
